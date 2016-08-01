47376e35b42bba6dbddd24df761d64b1bcb80425
eladnachum <seladnac@gmail.com>
2016-08-02 00:46:48 +0300

    Update JSApp.js

--- JSApp.js
+++ JSApp.js
@@ -9,12 +9,14 @@ var html1 ='<h1>' + 'IBM Tweets Windows' + '</h1>';
 
 var html2 = '<div style="width: 1000px; height: 300px; background-color: lightblue">' + 'Tweets....' + createTable(tweets)+'</div>';
 
-var html3 = '<br>' +'LastUpdated: ' +  TheDate() + '<br>';
+
 
 function run() {
     document.getElementById('test1').innerHTML = html1;
+    //find a way to update tweets
     document.getElementById('test2').innerHTML = html2;
-	document.getElementById('test3').innerHTML = html3;
+    var html3 = '<br>' +'LastUpdated: ' +  TheDate() + '<br>';
+    document.getElementById('test3').innerHTML = html3;
 }
 // Run everything when the document loads.
 //window.onload = run;
